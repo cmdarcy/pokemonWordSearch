c37503ca6bc06162de453c4fb890e89e8a79f44f
Christopher d'Arcy <cmdarcy89@gmail.com>
2024-05-20 19:28:00 -0400

    add display of fetched pokemon to board component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,8 +2,14 @@ import styles from "./Board.module.css";
 import { Link } from "react-router-dom";
 
 function Board() {
+	const pokemon = JSON.parse(localStorage.getItem("PokemonList"));
 	return (
 		<>
+			<ul>
+				{pokemon.map((pokemon) => (
+					<li>{pokemon}</li>
+				))}
+			</ul>
 			<Link to="/home">Back to Home</Link>
 		</>
 	);
