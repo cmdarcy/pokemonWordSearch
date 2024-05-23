7a56c6194c56b17dbac586c9e9b3e146a077ffa7
Christopher d'Arcy <cmdarcy89@gmail.com>
2024-05-23 11:14:07 -0400

    add button interactivity styling

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,12 +1,27 @@
 import styles from "./Square.module.css";
+import { useState } from "react";
 
-function Square({ letter, associatedPokemon }) {
+function Square({ letter, associatedPokemon, coordinates }) {
+	const [selected, setSelected] = useState(false);
+	function handleClick() {
+		setSelected(!selected);
+	}
 	return (
 		<>
 			{associatedPokemon ? (
-				<h3 className={styles.pokemonLetterbox}>{letter}</h3>
+				<button
+					className={selected ? styles.selected : styles.pokemonLetterbox}
+					onClick={handleClick}
+				>
+					{letter}
+				</button>
 			) : (
-				<h3 className={styles.letterbox}>{letter}</h3>
+				<button
+					className={selected ? styles.selected : styles.letterbox}
+					onClick={handleClick}
+				>
+					{letter}
+				</button>
 			)}
 		</>
 	);
