955daa8efde1cd22c65179b16215a9db093fec9a
Christopher d'Arcy <cmdarcy89@gmail.com>
2024-10-05 22:43:18 -0400

    style pokemon options list

--- src/components/PokemonOption/PokemonOption.jsx
+++ src/components/PokemonOption/PokemonOption.jsx
@@ -1,12 +1,13 @@
+import styles from "./PokemonOption.module.css";
 function PokemonOption({ pokemon, id = 1 }) {
 	return (
-		<div>
+		<div className={styles.flex_container}>
 			<p datatype={pokemon}>{pokemon}</p>
 			<img
 				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
 				alt=""
 			/>
-			<audio controls>
+			<audio className={styles.audio} controls>
 				<source
 					src={`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${id}.ogg`}
 					type="audio/ogg"
